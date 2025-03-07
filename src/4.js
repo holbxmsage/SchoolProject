e8d67eb7b982ad4f6bbd333656f89463d295b930
holbxmsage <holbxmsage@gmx.com>
2025-03-07 18:55:11 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,11 @@
+// Using express to create a server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
